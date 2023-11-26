fe64afa0dce5b8e6567294d35994ab03f96f05c8
louie <louiecasula@gmail.com>
2023-11-26 18:15:31 -0500

    Missed some implementations.

--- src/main/java/com/zipcodewilmington/froilansfarm/crops/Field.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/Field.java
@@ -1,6 +1,5 @@
 package com.zipcodewilmington.froilansfarm.crops;
 
-import com.sun.org.apache.xml.internal.security.keys.storage.StorageResolver;
 import com.zipcodewilmington.froilansfarm.structures.Storage;
 
 public class Field extends Storage {
--- src/test/java/com/zipcodewilmington/froilansfarm/weekdays/EveryMorningTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/weekdays/EveryMorningTest.java
@@ -10,58 +10,58 @@ public class EveryMorningTest extends BaseDayTest{
         public void DailyRoutineTest() {
             for (Horse h : stable1) {
                 froilan.mount(h);
-                froilan.ride(h);
+                h.move();
                 froilan.dismount(h);
                 Boolean expected1 = h.eat(3, "EarCorn");
                 Assert.assertTrue(expected1);
                 Assert.assertFalse(h.eat(3, "Tomato"));
             }
-            Assert.assertEquals(91, silo.getEdible("EarCorn"));
+            Assert.assertEquals(91, silo.getObject("EarCorn"));
             for (Horse h : stable2) {
                 froilanda.mount(h);
-                froilanda.ride(h);
+                h.move();
                 froilanda.dismount(h);
                 h.eat(3, "EarCorn");
             }
-            Assert.assertEquals(82, silo.getEdible("EarCorn"));
+            Assert.assertEquals(82, silo.getObject("EarCorn"));
             for (Horse h : stable3) {
                 froilan.mount(h);
-                froilan.ride(h);
+                h.move();
                 froilan.dismount(h);
                 h.eat(3, "EarCorn");
             }
-            Assert.assertEquals(70, silo.getEdible("EarCorn"));
+            Assert.assertEquals(70, silo.getObject("EarCorn"));
             for (Chicken c : chickenCoop1) {
                 c.eat(2, "Bean");
             }
-            Assert.assertEquals(94, silo.getEdible("Bean"));
+            Assert.assertEquals(94, silo.getObject("Bean"));
             for (Chicken c : chickenCoop2) {
                 c.eat(2, "Bean");
             }
-            Assert.assertEquals(86, silo.getEdible("Bean"));
+            Assert.assertEquals(86, silo.getObject("Bean"));
             for (Chicken c : chickenCoop3) {
                 c.eat(2, "Bean");
             }
-            Assert.assertEquals(78, silo.getEdible("Bean"));
+            Assert.assertEquals(78, silo.getObject("Bean"));
             for (Chicken c : chickenCoop4) {
                 c.eat(2, "Bean");
             }
-            Assert.assertEquals(70, silo.getEdible("Bean"));
+            Assert.assertEquals(70, silo.getObject("Bean"));
             froilan.eat(1, "EarCorn");
-            Assert.assertEquals(69, silo.getEdible("EarCorn"));
+            Assert.assertEquals(69, silo.getObject("EarCorn"));
             froilan.eat(2, "Tomato");
-            Assert.assertEquals(8, silo.getEdible("Tomato"));
+            Assert.assertEquals(8, silo.getObject("Tomato"));
             froilan.eat(5, "Egg");
-            Assert.assertEquals(20, silo.getEdible("Egg"));
+            Assert.assertEquals(20, silo.getObject("Egg"));
             froilan.eat(1, "Bean");
-            Assert.assertEquals(69, silo.getEdible("Bean"));
+            Assert.assertEquals(69, silo.getObject("Bean"));
             froilanda.eat(2, "EarCorn");
-            Assert.assertEquals(67, silo.getEdible("EarCorn"));
+            Assert.assertEquals(67, silo.getObject("EarCorn"));
             froilanda.eat(1, "Tomato");
-            Assert.assertEquals(7, silo.getEdible("Tomato"));
+            Assert.assertEquals(7, silo.getObject("Tomato"));
             froilanda.eat(2, "Egg");
-            Assert.assertEquals(18, silo.getEdible("Egg"));
+            Assert.assertEquals(18, silo.getObject("Egg"));
             froilanda.eat(3, "Bean");
-            Assert.assertEquals(66, silo.getEdible("Bean"));
+            Assert.assertEquals(66, silo.getObject("Bean"));
         }
 }
