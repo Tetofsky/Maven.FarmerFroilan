9790ba05a0115e6eeb79ab98fb60ea4c05707560
louie <louiecasula@gmail.com>
2023-11-26 15:59:19 -0500

    Added a missing bracket, indentation fix

--- src/test/java/com/zipcodewilmington/froilansfarm/weekdays/TuesdayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/weekdays/TuesdayTest.java
@@ -13,19 +13,19 @@ public class TuesdayTest extends BaseDayTest{
         //setup();
         Boolean actual = tractor.operate();
         Assert.assertTrue(actual);
+    }
 
-        @Test
-        public void Tuesday () {
-            tractor1.harvest(cropRow1);
-            Assert.assertEquals(169, silo.getEdible("EarCorn"));
-            tractor1.harvest(cropRow2);
-            Assert.assertEquals(22, silo.getEdible("Tomato"));
-            tractor1.harvest(cropRow3);
-            Assert.assertEquals(169, silo.getEdible("Bean"));
-            tractor1.harvest(cropRow4);
-            Assert.assertEquals(238, silo.getEdible("EarCorn"));
-            tractor1.harvest(cropRow5);
-            Assert.assertEquals(238, silo.getEdible("Bean"));
-        }
+    @Test
+    public void Tuesday() {
+        tractor1.harvest(cropRow1);
+        Assert.assertEquals(169, silo.getEdible("EarCorn"));
+        tractor1.harvest(cropRow2);
+        Assert.assertEquals(22, silo.getEdible("Tomato"));
+        tractor1.harvest(cropRow3);
+        Assert.assertEquals(169, silo.getEdible("Bean"));
+        tractor1.harvest(cropRow4);
+        Assert.assertEquals(238, silo.getEdible("EarCorn"));
+        tractor1.harvest(cropRow5);
+        Assert.assertEquals(238, silo.getEdible("Bean"));
     }
 }
