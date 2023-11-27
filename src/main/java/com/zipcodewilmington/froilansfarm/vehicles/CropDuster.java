2daa37d0fd9ebfd35a9acb8a20841e035e00629c
Gowri <gowri@Zipcoders-MacBook-Pro-64.local>
2023-11-27 14:13:14 -0500

    Vehicle code written

--- src/main/java/com/zipcodewilmington/froilansfarm/vehicles/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/vehicles/CropDuster.java
@@ -2,31 +2,41 @@ package com.zipcodewilmington.froilansfarm.vehicles;
 
 
 import com.zipcodewilmington.froilansfarm.creatures.Pilot;
+import com.zipcodewilmington.froilansfarm.crops.Crop;
+import com.zipcodewilmington.froilansfarm.crops.CropRow;
 import com.zipcodewilmington.froilansfarm.crops.Field;
 
+import java.util.Random;
+
 public class CropDuster <RiderType extends Pilot> extends Vehicle implements AirCraft, FarmVehicle {
+
+
     @Override
     public String makeNoise() {
-        return null;
+        return "Flapp!";
     }
 
     @Override
     public Boolean move() {
-        return null;
+        return true;
     }
 
 
     @Override
     public Boolean fly(Field field) {
-        return null;
+        return true;
     }
 
     @Override
     public Boolean operate() {
-        return null;
-    }
-
-    public Boolean fertilize() {
         return true;
     }
+
+    public boolean fertilize(CropRow cropRow) {
+        for (Crop c : cropRow) {
+            c.hasBeenFertilized = true;
+        }
+        return true;
+    }
+
 }
