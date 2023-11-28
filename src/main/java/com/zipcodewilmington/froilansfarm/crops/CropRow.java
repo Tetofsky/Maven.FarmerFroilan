aa5f60dd4018a9edebb82b657debbaf29aebab60
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-11-28 11:27:27 -0500

    changed getObject

--- src/main/java/com/zipcodewilmington/froilansfarm/crops/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/CropRow.java
@@ -5,6 +5,6 @@ import com.zipcodewilmington.froilansfarm.structures.Storage;
 public class CropRow <P extends Crop> extends Storage {
     @Override
     public Object getObject(Object object) {
-        return null;
+        return object;
     }
 }
